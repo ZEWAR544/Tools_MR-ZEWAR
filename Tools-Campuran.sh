913be84a03d89fb4af3f668c0051f0163c55c524
ZEWAR544 <72311881+ZEWAR544@users.noreply.github.com>
2020-10-04 09:44:07 +0700

    Create Tools-Campuran.sh

---
+++ Tools-Campuran.sh
@@ -0,0 +1,186 @@
+#!/bin/bash
+clear
+# Variables
+b='\033[1m'
+u='\033[4m'
+bl='\E[30m'
+r='\E[31m'
+g='\E[32m'
+bu='\E[34m'
+m='\E[35m'
+c='\E[36m'
+w='\E[37m'
+endc='\E[0m'
+enda='\033[0m'
+blue='\e[1;34m'
+cyan='\e[1;36m'
+red='\e[1;31m'
+
+figlet MR.ZEWAR XPLOIT  | lolcat
+
+echo "_____________________________________________________________" | lolcat
+echo  " AUTOR  =MR•Zewar" | lolcat
+echo  " IG     =MR•ZEWAR" | lolcat
+echo  " YOUTUBE=MR ZEWAR XPLOIT" | lolcat
+echo  "_____________________________________________________________" | lolcat
+echo ""
+echo -e "============================" | lolcat
+echo -e $b "1. Install DARK-FB${enda}";
+echo -e "============================" | lolcat
+echo -e $b "2. Install TUAN BADUT${enda}";
+echo -e "============================" | lolcat
+echo -e $b "3. Install ULTAH${enda}";
+echo -e "============================" | lolcat
+echo -e $b "4. Install ddos${enda}";
+echo -e "============================" | lolcat
+echo -e $b "5. Install HACK-FB${enda}";
+echo -e "============================" | lolcat
+echo -e $b "6. Hack CCTV${enda}";
+echo -e "============================" | lolcat
+echo -e $b "7. SC  TUAN BADUT${enda}";
+echo -e "============================" | lolcat
+echo -e $b "8. DDOS ATTACK TROJAN${enda}";
+echo -e "============================" | lolcat
+echo -e $b "9. Hack FB No Login${enda}";
+echo -e "============================" | lolcat
+echo -e $b "10. DDOS ATTACK${enda}";
+echo -e "============================" | lolcat
+echo -e $b "11. Brutal_Spammer${enda}";
+echo -e "============================" | lolcat
+echo -e $b "12. Deface Webdav${enda}";
+echo -e "============================" | lolcat
+echo -e $b "13. DDOS MUSLIM CYBER ARMY${enda}";
+echo -e "============================" | lolcat
+echo -e $b "14. Hack FB${enda}";
+echo -e "============================" | lolcat
+echo -e $b "00. Exit${enda}";
+echo -e "============================" | lolcat
+echo -e "╭─[PILIH NOMORNYA]"
+read -p "   ╰─root@ZEWAR=" pil;
+
+# dark-fb
+
+if [ $pil = "1" ]
+then
+        git clone https://github.com/V4N654T/dark-fb
+        cd dark-fb
+        python2 da.py
+
+
+
+#TOOLS TUAN B4DUT
+elif [ $pil = "2" ]
+then
+        git clone https://github.com/TUANB4DUT/TOOLSINSTALLERv4
+        cd TOOLSINSTALLERv4
+        bash TUANB4DUT.sh
+
+
+#Install ULTAH
+elif [ $pil = "3" ]
+then
+        git clone https://github.com/4NK3R-PRODUCT1ON/ULTAH
+        cd ULANG-TAHUN
+        python2 ultah.py
+
+
+#Install DDOS
+elif [ $pil = "4" ]
+then
+        git clone https://github.com/4NK3R-PRODUCT1ON/ddos
+        cd ddos
+        python hammer.py
+
+
+#Install HACK-FB
+elif [ $pil = "5" ]
+then
+        git clone https://github.com/pashayogi/SETAN
+        cd SETAN
+        python2 SETAN.py
+
+
+#Hack CCTV
+elif [ $pil = "6" ]
+then
+        git clone https://github.com/4NK3R-PRODUCT1ON/CCTV
+        cd CCTV
+        python2 cctv-4NK3R.py
+
+
+#SC TUAN BADUT
+elif [ $pil = "7" ]
+then
+       git clone https://github.com/MRCRO67/Lobaey
+       cd Lobaey
+       sh TUANB4DUT.sh
+
+
+#DDOS ATTACK TROJAN
+elif [ $pil = "8" ]
+then
+       git clone https://github.com/ZK1Hacker/TORJAN
+       cd TORJAN
+       python2 torjancyber.py
+
+
+#Hack FB No Login
+elif [ $pil = "9" ]
+then
+       git clone https://github.com/BotolMehedi/bhot
+       cd bhot
+       python2 bhot.py
+
+
+#DDOS ATTACK
+elif [ $pil = "10" ]
+then
+       git clone https://github.com/cyweb/hammer
+       cd hammer
+       python3 hammer.py
+
+
+#Brutal_Spammer
+elif [ $pil = "11" ]
+then
+       git clone https://github.com/IL4NGQW3R/brutalspammer
+       cd brutalspammer
+       python3 BrutalSpammer.py
+
+
+#Deface Webdav
+elif [ $pil = "12" ]
+then
+       git clone https://github.com/ZEWAR544/Webdav
+       cd Webdav
+       sh Webdav2.sh
+
+
+#DDOS MUSLIM CYBER ARMY
+elif [ $pil = "13" ]
+then
+       git clone https://github.com/ZEWAR544/Muslim-Ddos
+       cd Muslim-Ddos
+       python2 MUSLIM_DDOS.py
+
+
+#Hack FB
+elif [ $pil = "14" ]
+then
+       git clone https://github.com/ZEWAR544/Kumpulan-HackFB
+       cd Kumpulan-HackFB
+       sh siap.sh
+
+
+
+
+
+
+elif [ $pil = "00" ]
+then
+        clear
+        figlet Dadah | lolcat
+        echo "MR•ZEWAR"
+
+
+fi
